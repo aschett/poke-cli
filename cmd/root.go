11a1f87bc2fa34da23724deb432375e3de0713b5
aschett <a.schett@outlook.de>
2025-09-02 15:32:24 +0200

    Update short Description

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "poke-cli",
-	Short: "A brief description of your application",
+	Short: "A CLI-Tool to interact with the PokeAPI",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
